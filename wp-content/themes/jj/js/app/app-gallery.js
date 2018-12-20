2b822b63b84d4582adace40ece4177a4f26949f8
El_Professor <denispeterson96@gmail.com>
2018-12-21 00:20:21 +0300

    Deleted More Unnecessary files

--- wp-content/themes/jj/js/app/app-gallery.js
+++ wp-content/themes/jj/js/app/app-gallery.js
@@ -1,41 +1,41 @@
 app.gallery = {
 
-    init:function(){
+  init:function(){
 
 
-            $(".magicwall").magicWall({
-                maxItemHeight: 300,
-                maxItemWidth: 300,
-                delay: 400,
-                preloadBeforeSwitch: true,
-                loadingMode:"chain",
-                pauseOnHover: "item",
-                animations: "flipY,rollOutX,-rollOutX,rollOutY,-rollOutY,slideColumn,-slideColumn,slideRow,-slideRow,fade",
-                duration:800
+    $(".magicwall").magicWall({
+      maxItemHeight: 300,
+      maxItemWidth: 300,
+      delay: 400,
+      preloadBeforeSwitch: true,
+      loadingMode:"chain",
+      pauseOnHover: "item",
+      animations: "flipY,rollOutX,-rollOutX,rollOutY,-rollOutY,slideColumn,-slideColumn,slideRow,-slideRow,fade",
+      duration:800
 
-            });
+    });
 
-            $(".colorbox").colorbox({
-                maxWidth:"90%",
-                maxHeight:"90%",
-                onOpen: function(){
-                    $(".magicwall").magicWall("stop");
-                },
+    $(".colorbox").colorbox({
+      maxWidth:"90%",
+      maxHeight:"90%",
+      onOpen: function(){
+        $(".magicwall").magicWall("stop");
+      },
 
-                onClosed: function(){
-                    $(".magicwall").magicWall("start");
-                }
-            });
+      onClosed: function(){
+        $(".magicwall").magicWall("start");
+      }
+    });
 
-              alertify.log(msg5);
+    alertify.log(msg5);
 
-              setTimeout(function(){
-                  alertify.log(msg6);
-              },2000);
-
-
+    setTimeout(function(){
+      alertify.log(msg6);
+    },2000);
 
 
-    }
+
+
+  }
 
 }
