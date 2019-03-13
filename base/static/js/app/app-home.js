56e8db8ef92ea52b3a3cefbc4dedbddd07afed79
Dennis Peterson <denispeterson96@gmail.com>
2019-03-13 20:28:20 +0300

    created app-awards, app-resume

--- base/static/js/app/app-home.js
+++ base/static/js/app/app-home.js
@@ -1,114 +1,107 @@
 app.home = {
 
-    init:function(){
+  init:function(){
 
-		TweenMax.to( document.querySelector('.bg'), 0.4 ,{opacity:1 ,ease: Power2.easeIn});
+    TweenMax.to( document.querySelector('.bg'), 0.4 ,{opacity:1 ,ease: Power2.easeIn});
 
+    var mySVG = $('svg').drawsvg({duration:8000});
 
+    mySVG.drawsvg('animate');
 
-		var mySVG = $('svg').drawsvg({duration:8000});
-
-		mySVG.drawsvg('animate');
-
-		TweenMax.fromTo($(".jb")[0], 1.4, {opacity:0,y:0, ease:Power2.easeIn},{opacity:1,y:0,delay:1.6});
+    TweenMax.fromTo($(".jb")[0], 1.4, {opacity:0,y:0, ease:Power2.easeIn},{opacity:1,y:0,delay:1.6});
 
 
-        $('#nav_bar nav a').removeClass('active');
-        $('.home-link').addClass('active');
+    $('#nav_bar nav a').removeClass('active');
+    $('.home-link').addClass('active');
 
-        $(".home-page h1").blast({
-            delimiter: "character",
-            tag: "span"
-        });
+    $(".home-page h1").blast({
+      delimiter: "character",
+      tag: "span"
+    });
 
-
-
-
-        a = 0;
-        b= 0;
-        $(".home-page .blast").each(function(){
+    a = 0;
+    b= 0;
+    $(".home-page .blast").each(function(){
 
 
-            if(a==300){
+      if(a==300){
 
-                a=400;
+        a=400;
 
-            }
+      }
 
-            if(a==1100){
+      if(a==1100){
 
-                a=1200;
+        a=1200;
 
-            }
-
-
-            var el = $(this);
-
-            if(a==400){
-
-                setTimeout(function(){
-
-                    $(".home-page h1 img").addClass('animation-logo');
+      }
 
 
-                },800);
-            }
+      var el = $(this);
 
-            setTimeout(function(){
+      if(a==400){
 
-                el.addClass('animated bounceIn');
-
-
-
-
-            },a);
-
-            if(a < 1200) {
-                a = a + 100;
-            }else {
-
-                a = a + 80;
-
-            }
-
-
-
-        });
         setTimeout(function(){
 
-            $(".home-page .blast").removeClass('animated bounceIn');
-            $(".home-page .blast").css('opacity',1);
-
-            $(".home-page .blast").mouseenter(function (){
-
-                var el = $(this);
-
-                $(this).addClass('animated rubberBand');
-                $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-
-                    el.removeClass('animated rubberBand');
-
-                });
-
-            });
+          $(".home-page h1 img").addClass('animation-logo');
 
 
-        },3000);
-        $(".home-page .flat-button").mouseenter(function (){
+        },800);
+      }
 
-            var el = $(this);
+      setTimeout(function(){
+
+        el.addClass('animated bounceIn');
 
 
-            $(this).addClass('animated rubberBand');
-            $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 
-                el.removeClass('animated rubberBand');
 
-            });
+      },a);
+
+      if(a < 1200) {
+        a = a + 100;
+      }else {
+
+        a = a + 80;
+
+      }
+
+    });
+    setTimeout(function(){
+
+      $(".home-page .blast").removeClass('animated bounceIn');
+      $(".home-page .blast").css('opacity',1);
+
+      $(".home-page .blast").mouseenter(function (){
+
+        var el = $(this);
+
+        $(this).addClass('animated rubberBand');
+        $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+
+          el.removeClass('animated rubberBand');
 
         });
 
+      });
 
-    }
+
+    },3000);
+    $(".home-page .flat-button").mouseenter(function (){
+
+      var el = $(this);
+
+
+      $(this).addClass('animated rubberBand');
+      $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+
+        el.removeClass('animated rubberBand');
+
+      });
+
+    });
+
+
+  }
 
 }
