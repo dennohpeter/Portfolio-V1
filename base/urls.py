dd4bccb720eab99f96b5380a94598b789f22c42e
Dennis Peterson <denispeterson96@gmail.com>
2019-04-19 23:25:25 +0300

    Trying to make website one page

--- base/urls.py
+++ base/urls.py
@@ -3,11 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^skills/$', views.skills, name='skills'),
-    url(r'^resume/$', views.resume, name='resume'),
-    url(r'^portfolio/$', views.portfolio, name='portfolio'),
-    url(r'^awards/$', views.awards, name='awards'),
-    url(r'^contact/$', views.contact, name='contact'),
-    url(r'^feed/$', views.feed, name='feed'),
     ]
