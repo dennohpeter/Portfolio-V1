42a131097772d65d53dcf23d3d910c3c33fa3677
Dennis Peterson <denispeterson96@gmail.com>
2019-06-02 14:05:11 +0300

    refactoring portfolio

--- base/views.py
+++ base/views.py
@@ -3,31 +3,3 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, "index.html")
-
-
-def about(request):
-    return render(request, "about.html")
-
-
-def skills(request):
-    return render(request, "skills.html")
-
-
-def resume(request):
-    return render(request, "resume.html")
-
-
-def portfolio(request):
-    return render(request, "portfolio.html")
-
-
-def awards(request):
-    return render(request, "awards.html")
-
-
-def contact(request):
-    return render(request, "contact.html")
-
-
-def feed(request):
-    return redirect("http://dennohpeter.pythonanywhere.com")
