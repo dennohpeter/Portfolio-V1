99c2f6aa38b0f0d962e1daf4b3240af0b4350810
Dennis Peterson <denispeterson96@gmail.com>
2019-06-02 14:31:33 +0300

    trying to fix navigation problem

--- base/static/js/fullPage.js
+++ base/static/js/fullPage.js
@@ -1,13 +1,3 @@
-/*!
- * fullPage 3.0.5
- * https://github.com/alvarotrigo/fullPage.js
- *
- * @license GPLv3 for open source use only
- * or Fullpage Commercial License for commercial use
- * http://alvarotrigo.com/fullPage/pricing/
- *
- * Copyright (C) 2018 http://alvarotrigo.com/fullPage - A project by Alvaro Trigo
- */
 (function( root, window, document, factory, undefined) {
     if( typeof define === 'function' && define.amd ) {
         // AMD. Register as an anonymous module.
--- base/static/js/main.js
+++ base/static/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	menu: '#nav_bar nav',
 	lockAnchors: false,
 	anchors:['home', 'about', 'resume', 'projects', 'awards', 'contact'],
-	navigation: false,
+	navigation: true,
 	navigationPosition: 'right',
 	navigationTooltips: ['home', 'about', 'resume', 'projects', 'awards', 'contact'],
   sectionsColor:["#383C4A","#213C39","#FCE94F","","#75507B", "#CC0000"],
