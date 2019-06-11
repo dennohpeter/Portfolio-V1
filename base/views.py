5da416afd2e0d5e6191c08291beaf52255d99538
Dennis Peterson <denispeterson96@gmail.com>
2019-06-11 15:23:57 +0300

    fixing internal server error

--- base/views.py
+++ base/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, "index.html")
 
-def send_mail(request):
-    if request.method == "POST":
-        form_date = request.POST.get('')
+# def send_mail(request):
+#     if request.method == "POST":
+#         form_date = request.POST.get('')
