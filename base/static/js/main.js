cd7ff3b6eb54ac73b8286b927e5972b0a672dc6a
Dennis Peterson <denispeterson96@gmail.com>
2019-08-04 13:23:56 +0300

    added notice modal

--- base/static/js/main.js
+++ base/static/js/main.js
@@ -2,3 +2,18 @@ function show_menu() {
     var element = document.querySelector('#nav_bar #menu');
     element.classList.toggle('show');
 }  
+var notice = document.getElementById("copyright-notice");
+var show = document.getElementById("notice");
+var close = document.getElementsByClassName("close")[0];
+show.onclick = function() {
+  notice.style.display = "block";
+}
+close.onclick = function() {
+  notice.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == notice) {
+    notice.style.display = "none";
+  }
+}
