140fb38c536f78674a640b395c2a9922ab9c48c4
Dennis Peterson <denispeterson96@gmail.com>
2019-10-25 18:42:58 +0300

    added domain to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com", ".dennohpeter.com"]
 
 
 # Application definition
